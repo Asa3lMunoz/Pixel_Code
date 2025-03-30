54a31b949dbe678eb04a2c029de4ee99dc04f51d
Yisus01 <jesu.alejandrox@gmail.com>
2025-03-30 20:05:52 -0300

    Se crea componente  contacto y se hace la ruta respectiva hacia este, se instala Axios para API

---
+++ src/components/Contacto.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+function Contacto() {
+
+  // Definimos el estado para almacenar los datos de la API Propio de React
+const [datos, setDatos] = useState([]);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(null);
+
+
+const fetchMessages = async () => {
+  try {
+    //Solicitud GET a la API
+    const response = await axios.get('http://localhost:3000/api/v1/contactRequests');
+    if (response.data && response.data.success) {
+      setDatos(response.data.data); // Se Almacena los datos en el estado
+    }
+    setLoading(false);
+  } catch (err) {
+    setError('Error al obtener los datos');
+    setLoading(false);
+  }
+};
+
+// useEffect para llamar la API cuando el componente se monta
+useEffect(() => {
+  fetchMessages();
+}, []);
+
+if (loading) {
+  return <div>Cargando...</div>;
+}
+
+if (error) {
+  return <div>{error}</div>;
+}
+
+return (
+    <>
+
+      <div className='contenedorcontacto'>
+        
+        <table className='contenedorcontacto-table'>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Email</th>
+                    <th>Teléfono</th>
+                    <th>Mensaje</th>
+                    <th>Fecha Recepción</th>
+                    <th>Contestado</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {datos.map((datos, index) => (
+                <tr key={index}>
+                    <td>{datos.firstName}</td>
+                    <td>{datos.lastName}</td>
+                    <td>{datos.email}</td>
+                    <td>{datos.phone}</td>
+                    <td>{datos.message}</td>
+
+                </tr>
+                ))}
+            </tbody>
+        </table>
+
+      </div>
+
+    </>
+
+);
+}
+
+export default Contacto
--- src/router.jsx
+++ src/router.jsx
@@ -4,6 +4,7 @@ import Ingresar from './views/IngresarPage'
 import FormularioSignup from './components/FormularioSignup'
 import FormularioIngresarPage from './components/FormularioIngresarPage'
 import FormularioForgot from './components/FormularioForgot'
+import Contacto from './components/Contacto'
 export default function AppRouter() {
   return (
 
@@ -14,6 +15,7 @@ export default function AppRouter() {
             <Route path='/FormularioSignup' element={<FormularioSignup/>} />
             <Route path='/FormularioIngresarPage' element={<FormularioIngresarPage/>} />
             <Route path='/FormularioForgot' element={<FormularioForgot/>} />
+            <Route path='/Contacto' element={<Contacto/>} />
         </Routes>
     </BrowserRouter>
 
